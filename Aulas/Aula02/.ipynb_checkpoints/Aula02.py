4a81ccf7a168e9194c2d25e971c47bf8ac7d339f
Thalyson Ribeiro de Araújo <thalysonribeiro5@gmail.com>
2023-01-20 01:23:41 -0300

    Commit

---
+++ Aulas/Aula02/.ipynb_checkpoints/Aula02.py
@@ -0,0 +1,11 @@
+"""
+    Comentário de multiplas linhas
+"""
+#Tudo recebe 0, basta colocar uma variavel na outr
+num1=num2=res=0
+canal="CFB Cursos"
+curso="Curso de Python"
+print(canal + " - " + curso)
+
+if 10 > 2:
+    print("Maior")
---
+++ Aulas/Aula03/Aula03.py
@@ -0,0 +1,10 @@
+num1=num2=res=0
+
+#def significa função, ou seja, retornar um comando específico.
+#variáveis globalis dentro de uma função "def"
+def cn():
+    global canal
+    canal = "CFB Cursos"
+
+cn()
+print(canal)
